b65aa4f51bfd8f48304179a5affa2e7ac14970ea
YaroslavW <doctor355@yahoo.com>
2019-02-11 13:32:54 +0300

    Asinc request to the date for table

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+  async componentDidMount() {
+    const response = await fetch(` http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`)
+    const data = await response.json()
+    console.log(data)
+
+  }
   render() {
     return (
       <div className="container">
