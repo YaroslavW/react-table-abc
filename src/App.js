3802a77f3f3b1083046b0da2878f53dea403b2c2
YaroslavW <doctor355@yahoo.com>
2019-02-12 10:46:05 +0300

    Sort items in columns and display sorting options in the header.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Loader from './Loader/Loader';
 import Table from './Table/Table';
+import _ from 'lodash';
 
 class App extends Component {
   state ={
     isLoading: true,
     data: [],
+    sort: 'asc',  // 'desc'
+    sortField: 'id',
   }
   async componentDidMount() {
     const response = await fetch(` http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`)
@@ -13,9 +16,21 @@ class App extends Component {
     // console.log(data)
     this.setState({
       isLoading: false,
-      data
+      data: _.orderBy(data, this.state.sortField, this.state.sort)
     })
 
+  }
+  onSort = sortField => {
+    
+    const cloneData = this.state.data.concat();
+    const sortType = this.state.sort === 'asc' ? 'desc' : 'asc';
+    const orderedData = _.orderBy(cloneData, sortField, sortType);
+
+    this.setState({
+      data: orderedData,
+      sort: sortType,
+      sortField
+    })
   }
   render() {
     return (
@@ -25,6 +40,9 @@ class App extends Component {
         ? <Loader />
         : <Table 
         data={this.state.data}
+        onSort={this.onSort}
+        sort={this.state.sort}
+        sortField={this.state.sortField}
         />
       }
       </div>
--- src/Table/Table.js
+++ src/Table/Table.js
@@ -4,11 +4,21 @@ export default props => (
     <table className="table">
         <thead>
             <tr>
-                <th>ID</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>E-mail</th>
-                <th>Phone</th>
+                <th onClick={props.onSort.bind(null, 'id')}>
+                    ID {props.sortField === 'id' ? <small>{props.sort}</small> : null}
+                </th>
+                <th onClick={props.onSort.bind(null, 'firstName')}>
+                    First Name {props.sortField === 'firstName' ? <small>{props.sort}</small> : null}
+                </th>
+                <th onClick={props.onSort.bind(null, 'lastName')}>
+                    Last Name {props.sortField === 'lastName' ? <small>{props.sort}</small> : null}
+                </th>
+                <th onClick={props.onSort.bind(null, 'email')}>
+                    E-mail {props.sortField === 'email' ? <small>{props.sort}</small> : null}
+                </th>
+                <th onClick={props.onSort.bind(null, 'phone')}>
+                    Phone {props.sortField === 'phone' ? <small>{props.sort}</small> : null}
+                </th>
             </tr>
         </thead>
         <tbody>
