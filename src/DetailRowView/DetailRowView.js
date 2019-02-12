c6f02218ea8d8ee8380e91959b4d218b0fe652b8
YaroslavW <doctor355@yahoo.com>
2019-02-12 14:16:32 +0300

    data output from the table row to the page

--- src/DetailRowView/DetailRowView.js
+++ src/DetailRowView/DetailRowView.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-export default () => (
+export default ({person}) => (
     <div>
-        <h1>DetailRowView</h1>
-    </div>
+    <p>Выбран пользователь <b>{person.firstName + ' ' + person.lastName}</b></p>
+    <p>
+    Описание: <br />
+    <textarea defaultValue={person.description} />
+    </p>
+
+    <p>Адрес проживания: <b>{person.address.streetAddress}</b></p>
+    <p>Город: <b>{person.address.city}</b></p>
+    <p>Провинция/штат: <b>{person.address.state}</b></p>
+    <p>Индекс: <b>{person.address.zip}</b></p>
+
+  </div>
 )
